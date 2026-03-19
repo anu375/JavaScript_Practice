fae0bb42ea7512315d5760ce93b77e10ca243557
anu <anujadhav32004@gmail.com>
2026-03-19 16:39:06 +0530

    Pattern_Practice_IN_JS

---
+++ first.js
@@ -0,0 +1,24 @@
+//Square - Pattern
+
+// let n = 4;
+// for (let i = 1; i <= n; i++) {
+//     let row = "";
+//     for (let j = 1; j <= n; j++) {
+//         row += "*";
+//     }
+//     console.log(row);
+// }
+
+
+
+//Right - Triangle
+
+let n = 4;
+
+for (let i = 1; i <= n; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+        row += "*";
+    }
+    console.log(row);
+}
